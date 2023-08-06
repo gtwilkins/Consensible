e37cf3657f9a5148c164f1b3619bf17bf6b0c1b0
gtwilkins <glen.t.wilkins@gmail.com>
2023-08-07 03:39:29 +1000

    Stable

--- src/commands/index.cpp
+++ src/commands/index.cpp
@@ -37,6 +37,7 @@ Index::Index( Arguments& args )
     if ( args.reindex_ || ( !canResume && !isComplete ) )
     {
         Transform::load( fns, args.inputs_, doRevComp );
+        Transform::run( fns );
         IndexWriter idx( fns, 1024, 20000 );
     }
     else if ( canResume  )
@@ -167,88 +168,88 @@ Index::Index( Arguments& args )
 //    Transform::run( fns );
 //}
 
-void Index::newTransform( PreprocessFiles* fns, int minScore, ifstream &infile, bool revComp )
-{
-    uint8_t fileCount = 0, pairedLibCount = 0;
-    
-    if ( infile.is_open() && infile.good() )
-    {
-        vector< vector<ReadFile*> > libs;
-        string line;
-        int lineNum = 1;
-        ReadFile* readFile = NULL;
-        while( getline( infile, line ) )
-        {
-	    if ( line.empty() || line[0] == '#' ) continue;
-            vector<string> args;
-            
-            size_t it = line.find( ' ' );
-            while ( it != line.npos && args.size() < 4 )
-            {
-                args.push_back( line.substr( 0, it ) );
-                line = line.substr( it + 1 );
-                it = line.find( ' ' );
-            }
-            
-            args.push_back( line );
-            
-            if ( ( args.size() == 2 || args.size() == 3 ) && args[0] == "paired" )
-            {
-                vector<ReadFile*> lib;
-                readFile = new ReadFile( args[1], 0, minScore );
-                fileCount++;
-                lib.push_back( readFile );
-                if ( args.size() == 3 )
-                {
-                    readFile = new ReadFile( args[2], 0, minScore );
-                    fileCount++;
-                }
-                lib.push_back( readFile );
-                libs.push_back( lib );
-                pairedLibCount++;
-            }
-            else if ( args.size() == 2 && args[0] == "single" )
-            {
-                readFile = new ReadFile( args[1], 0, minScore );
-                fileCount++;
-                vector<ReadFile*> lib = { readFile };
-                libs.push_back( lib );
-            }
-            else
-            {
-                cerr << "Error: invalid input form in line " << lineNum << " of input file." << endl << endl;
-                cerr << "Expecting inputs to be in one of the following forms:" << endl;
-                cerr << "\tpaired [separated_pair_file_1] [separated_pair_file_2]" << endl;
-                cerr << "\tpaired [interleaved_pair_file]" << endl;
-                cerr << "\tsingle [unpaired_file]" << endl;
-                exit( EXIT_FAILURE );
-            }
-            
-            lineNum++;
-        }
-        
-        if ( pairedLibCount > 6 )
-        {
-            cerr << "Error: Excessive library count of " << (int)pairedLibCount << ". Maximum supported is 6." << endl;
-            exit( EXIT_FAILURE );
-        }
-        
-        cout << "Preprocessing step 1 of 3: reading input files..." << endl << endl;
-        Transform::load( fns, libs, pairedLibCount, revComp );
-        Transform::run( fns );
-    }
-    else
-    {
-        cerr << "Error: No input file provided." << endl;
-        exit( EXIT_FAILURE );
-    }
-}
-
-void Index::resumeTransform( PreprocessFiles* fns )
-{
-    cout << "Resuming preprocessing..." << endl << endl;
-    Transform::run( fns );
-}
+//void Index::newTransform( PreprocessFiles* fns, int minScore, ifstream &infile, bool revComp )
+//{
+//    uint8_t fileCount = 0, pairedLibCount = 0;
+//    
+//    if ( infile.is_open() && infile.good() )
+//    {
+//        vector< vector<ReadFile*> > libs;
+//        string line;
+//        int lineNum = 1;
+//        ReadFile* readFile = NULL;
+//        while( getline( infile, line ) )
+//        {
+//	    if ( line.empty() || line[0] == '#' ) continue;
+//            vector<string> args;
+//            
+//            size_t it = line.find( ' ' );
+//            while ( it != line.npos && args.size() < 4 )
+//            {
+//                args.push_back( line.substr( 0, it ) );
+//                line = line.substr( it + 1 );
+//                it = line.find( ' ' );
+//            }
+//            
+//            args.push_back( line );
+//            
+//            if ( ( args.size() == 2 || args.size() == 3 ) && args[0] == "paired" )
+//            {
+//                vector<ReadFile*> lib;
+//                readFile = new ReadFile( args[1], 0, minScore );
+//                fileCount++;
+//                lib.push_back( readFile );
+//                if ( args.size() == 3 )
+//                {
+//                    readFile = new ReadFile( args[2], 0, minScore );
+//                    fileCount++;
+//                }
+//                lib.push_back( readFile );
+//                libs.push_back( lib );
+//                pairedLibCount++;
+//            }
+//            else if ( args.size() == 2 && args[0] == "single" )
+//            {
+//                readFile = new ReadFile( args[1], 0, minScore );
+//                fileCount++;
+//                vector<ReadFile*> lib = { readFile };
+//                libs.push_back( lib );
+//            }
+//            else
+//            {
+//                cerr << "Error: invalid input form in line " << lineNum << " of input file." << endl << endl;
+//                cerr << "Expecting inputs to be in one of the following forms:" << endl;
+//                cerr << "\tpaired [separated_pair_file_1] [separated_pair_file_2]" << endl;
+//                cerr << "\tpaired [interleaved_pair_file]" << endl;
+//                cerr << "\tsingle [unpaired_file]" << endl;
+//                exit( EXIT_FAILURE );
+//            }
+//            
+//            lineNum++;
+//        }
+//        
+//        if ( pairedLibCount > 6 )
+//        {
+//            cerr << "Error: Excessive library count of " << (int)pairedLibCount << ". Maximum supported is 6." << endl;
+//            exit( EXIT_FAILURE );
+//        }
+//        
+//        cout << "Preprocessing step 1 of 3: reading input files..." << endl << endl;
+//        Transform::load( fns, libs, pairedLibCount, revComp );
+//        Transform::run( fns );
+//    }
+//    else
+//    {
+//        cerr << "Error: No input file provided." << endl;
+//        exit( EXIT_FAILURE );
+//    }
+//}
+//
+//void Index::resumeTransform( PreprocessFiles* fns )
+//{
+//    cout << "Resuming preprocessing..." << endl << endl;
+//    Transform::run( fns );
+//}
 
 void Index::printUsage()
 {
